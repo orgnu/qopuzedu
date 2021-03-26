0ad451416eda703e4092230f8f6b9a8f5695d42d
Nurlan Garash <68768916+nurlangarash@users.noreply.github.com>
2021-03-26 23:28:45 +0400

    Update main.go

--- back-end/main.go
+++ back-end/main.go
@@ -1,5 +1,3 @@
-// Copyright 2015 The Hugo Authors. All rights reserved.
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -16,7 +14,7 @@ package main
 import (
 	"os"
 
-	"github.com/gohugoio/hugo/commands"
+	"github.com/nurlangarash/qopuzedu/back-end/commands"
 )
 
 func main() {
